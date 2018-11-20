0634d2eb249463319f84b1c3e06d1a218bda0edf
Ed Phelps <edphelps1@gmail.com>
2018-11-20 14:31:59 -0700

    working redux UI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React, { Component } from 'react';
-import './App.css';
+// src/js/components/App.js
+import React from 'react';
+import List from './components/List';
+import Form from './components/Form';
 
-class App extends Component {
-
-  trash = () => {
-    console.log("nothing");
-  }
-
-  render() {
-    return (
-      <div className="App">
-        hello there
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="row mt-5">
+    <div className="col-sm-4 offset-sm-1">
+      <h2>Articles</h2>
+      <List />
+    </div>
+    <div className="col-sm-4 offset-sm-1">
+      <h2>Add a new article</h2>
+      <Form />
+    </div>
+  </div>
+);
 
 export default App;
+
+
+// import React, { Component } from 'react';
+// import './App.css';
+//
+// class App extends Component {
+//   trash = () => {
+//     console.log('nothing');
+//   }
+//
+//   render() {
+//     return (
+//       <div className="App">
+//         hello there
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-// added from tutorial -------------------------
+import { Provider } from 'react-redux';
 import store from './store/index';
-import { addArticle } from './actions/index';
+import App from './App';
 
-window.store = store;
-window.addArticle = addArticle;
-// ---------------------------------------------
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import store from './store/index';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+//
+// // added from tutorial -------------------------
+// // import { addArticle } from './actions/index';
+// //
+// // window.store = store;
+// // window.addArticle = addArticle;
+// // ---------------------------------------------
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+//
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+//
+// // added from tutorial -------------------------
+// import store from './store/index';
+// import { addArticle } from './actions/index';
+//
+// window.store = store;
+// window.addArticle = addArticle;
+// // ---------------------------------------------
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+//
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
